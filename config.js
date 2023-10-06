740e2374ab549f47e2ef55598a9fc5a6ac1deac9
unknown <paozin139@gmail.com>
2023-10-06 19:33:44 -0300

    adicionando o firebase

--- config.js
+++ config.js
@@ -2,7 +2,13 @@ import firebase from 'firebase';
 
 // adicione SDK do Firebase
 var firebaseConfig = {
-
+    apiKey: "AIzaSyC5ZKLzzEMdL2ai4xCD7-ZshvA7KOUDQeM",
+    authDomain: "projeto-c67-955b5.firebaseapp.com",
+    databaseURL: "https://projeto-c67-955b5-default-rtdb.firebaseio.com",
+    projectId: "projeto-c67-955b5",
+    storageBucket: "projeto-c67-955b5.appspot.com",
+    messagingSenderId: "775541620687",
+    appId: "1:775541620687:web:7f8ca43905f3c1bb3d5cba"
 };
 // Initialice o Firebase
 firebase.initializeApp(firebaseConfig);
